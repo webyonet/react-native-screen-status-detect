dd9be1efd6ece9c2307b6558866e8523c0935a93
G.Burak <webyonet@hotmail.com>
2021-08-27 22:47:47 +0300

    Update ScreenStatusDetectModule.java

--- android/src/main/java/com/screenstatusdetect/ScreenStatusDetectModule.java
+++ android/src/main/java/com/screenstatusdetect/ScreenStatusDetectModule.java
@@ -1,17 +1,34 @@
+
 package com.screenstatusdetect;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Display;
 import android.view.WindowManager;
+
 import androidx.annotation.RequiresApi;
+
 import com.facebook.react.bridge.*;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
 public class ScreenStatusDetectModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
@@ -78,6 +95,76 @@ public class ScreenStatusDetectModule extends ReactContextBaseJavaModule {
         }
     }
 
+    private String byte2HexFormatted(byte[] arr) {
+        StringBuilder str = new StringBuilder(arr.length * 2);
+
+        for (int i = 0; i < arr.length; i++) {
+            String h = Integer.toHexString(arr[i]);
+            int l = h.length();
+            if (l == 1) h = "0" + h;
+            if (l > 2) h = h.substring(l - 2, l);
+            str.append(h.toUpperCase());
+            if (i < (arr.length - 1)) str.append(':');
+        }
+
+        return str.toString();
+    }
+
+    @ReactMethod
+    @SuppressLint("PackageManagerGetSignatures")
+    public void getCertificateSHA1Fingerprint(Promise promise) {
+        PackageManager pm = reactContext.getPackageManager();
+        String packageName = reactContext.getPackageName();
+        int flags = PackageManager.GET_SIGNATURES;
+        PackageInfo packageInfo = null;
+
+        try {
+            packageInfo = pm.getPackageInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assert packageInfo != null;
+        Signature[] signatures = packageInfo.signatures;
+        byte[] cert = signatures[0].toByteArray();
+        InputStream input = new ByteArrayInputStream(cert);
+        CertificateFactory cf = null;
+
+        try {
+            cf = CertificateFactory.getInstance("X509");
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+
+        X509Certificate c = null;
+
+        try {
+            assert cf != null;
+            c = (X509Certificate) cf.generateCertificate(input);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+
+        String hexString = null;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            assert c != null;
+            byte[] publicKey = md.digest(c.getEncoded());
+            hexString = byte2HexFormatted(publicKey);
+        } catch (NoSuchAlgorithmException | CertificateEncodingException e1) {
+            e1.printStackTrace();
+        }
+
+        if (promise != null) {
+            WritableMap map = Arguments.createMap();
+
+            map.putString("fingerprint", hexString);
+
+            promise.resolve(map);
+        }
+    }
+
     @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private void createDisplayListener() {
         displayManager = (DisplayManager) reactContext.getSystemService(Context.DISPLAY_SERVICE);
@@ -113,7 +200,7 @@ public class ScreenStatusDetectModule extends ReactContextBaseJavaModule {
                 @Override
                 public void onChange(boolean selfChange, Uri uri) {
                     super.onChange(selfChange, uri);
-                    if(isVideoFile(uri)){
+                    if (isVideoFile(uri)) {
                         WritableMap map = Arguments.createMap();
 
                         map.putString("screenStatus", "VIDEO_RECORDING_DETECTED");
